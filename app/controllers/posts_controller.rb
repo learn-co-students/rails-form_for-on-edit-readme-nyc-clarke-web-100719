6b4d8dea37b1eb6cccad6d36063e8fce51c2bc5b
Jeffrey Paulino <jp7kode@gmail.com>
2019-11-06 11:58:12 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		# @post = Post.find(params[:id])
 	end
 
 	def new
@@ -20,12 +22,24 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
+		# @post = Post.find(params[:id])
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  # @post = Post.find(params[:id])
+	  # @post.update(title: params[:title], description: params[:description])
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
+
+  private
+
+  def post_params
+		params.require(:post).permit(:title, :description)
+	end
+
+  def set_post
+		@post = Post.find(params[:id])
+	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+50.times do
+  Post.create(
+          title: Faker::DcComics.title,
+          description: Faker::DcComics.title
+  )
+end
